f6744cbba2683af420fbe0aa3bdb6551fa2022a9
JordanLCM <93762869+JordanLCM@users.noreply.github.com>
2021-11-27 18:00:57 +0000

    Assignment 2 Update

--- AreaMeasurementOfShape.java
+++ AreaMeasurementOfShape.java
@@ -3,6 +3,7 @@ package assignment2;
 public class AreaMeasurementOfShape {
 	
 	public static void main(String[] args) throws InterruptedException {
+		int i[] = {1, 2, 3};
 		
 		String a = "Triangle";
 		
@@ -12,11 +13,20 @@ public class AreaMeasurementOfShape {
 		try {
 			System.out.println("Shape :" + a);
 			Thread.sleep(500);
+			System.out.println(i[3]);
+			//Starts from 0, 1, 2
 			System.out.println("Dimension Area : " + (LengthOfTriangle*HeightOfTriangle)/2 + "cm");
 		}
-		catch(Exception e){
+		catch(ArithmeticException e){
 			System.out.println("Unable to calculate");
 			e.printStackTrace();
+		}
+		catch(ArrayIndexOutOfBoundsException f){
+			System.out.println("Array out of bounds!");
+			f.printStackTrace();
+		}
+		finally {
+			System.out.println((LengthOfTriangle*HeightOfTriangle)/2  + "cm");
 		}
 			
 	}

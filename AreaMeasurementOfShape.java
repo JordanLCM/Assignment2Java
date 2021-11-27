4e0cde7c3c6e988665197af28f154cc94a54fa75
JordanLCM <93762869+JordanLCM@users.noreply.github.com>
2021-11-27 18:11:46 +0000

    Assignment 2 update 1.0

--- AreaMeasurementOfShape.java
+++ AreaMeasurementOfShape.java
@@ -4,31 +4,28 @@ public class AreaMeasurementOfShape {
 	
 	public static void main(String[] args) throws InterruptedException {
 		int i[] = {1, 2, 3};
-		
 		String a = "Triangle";
-		
-		int LengthOfTriangle = 12;
-		int HeightOfTriangle = 5;
-		
+		int LengthOfTriangle = 15;
+		//Input Length Value here
+		int HeightOfTriangle = 10;
+		//Input Height Value here
 		try {
 			System.out.println("Shape :" + a);
-			Thread.sleep(500);
-			System.out.println(i[3]);
-			//Starts from 0, 1, 2
+			Thread.sleep(1000);
+			//////System.out.println(i[1]);
+			//Starts from 0, 1, 2_ArrayIndexOutOfBoundsExceptionSituation
+			//////System.out.println(10/0);
+			//ArithmeticExecptionSituation
 			System.out.println("Dimension Area : " + (LengthOfTriangle*HeightOfTriangle)/2 + "cm");
-		}
-		catch(ArithmeticException e){
+			//Area of triangle = (Length X Height) divided by 2
+		}catch(ArithmeticException e){
 			System.out.println("Unable to calculate");
 			e.printStackTrace();
-		}
-		catch(ArrayIndexOutOfBoundsException f){
+		}catch(ArrayIndexOutOfBoundsException f){
 			System.out.println("Array out of bounds!");
 			f.printStackTrace();
-		}
-		finally {
+		}finally {
 			System.out.println((LengthOfTriangle*HeightOfTriangle)/2  + "cm");
 		}
-			
 	}
-
 }
